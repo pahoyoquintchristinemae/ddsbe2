dd3851d9f05d50c148b8c8ee4c6cbbc51838ecf3
pahoyoquintchristinemae <pahoyoquintchristinemae>
2021-04-19 18:43:48 +0800

    first commit

---
+++ app/User.php
@@ -0,0 +1,15 @@
+<?php
+
+ namespace App;
+
+ use Illuminate\Database\Eloquent\Model;
+
+    class User extends Model{
+        protected $table = 'tbluser';
+        // column sa table
+        protected $fillable = [
+            'username', 'passowrd'
+    ];
+ }
+
+ ?>
