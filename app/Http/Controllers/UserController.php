6cbd8dabe12f9b605aa2a528b17e7e0921c2d275
pahoyoquintchristinemae <pahoyoquintchristinemae>
2021-04-21 15:31:25 +0800

    Changes as of April 21, 2021

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,20 +2,114 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\Response;
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
-use App\User;
+use DB;
 
-    Class UserController extends Controller {
-        private $request;
+class UserController extends Controller
+{
+    use ApiResponser;
+    
+    private $request;
+    
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+        
+    }
 
-        public function __construct(Request $request){
-            $this->request = $request;
+
+    public function getUsers()
+    {
+        $users =  DB::connection('mysql')->select("Select * from tbluser");
+        return $this->successResponse($users);
+    }
+
+
+    public function index()
+    {
+        $users = User::all();
+
+        return $this->successResponse($users);
+    }
+
+
+    public function add(Request $request)
+    {
+        $rules = [
+
+            'username' => 'required|max:20',
+            'password' => 'required|max:20',
+            'gender' => 'required|in:Male,Female',
+
+        ];
+
+
+        $this->validate($request, $rules);
+        $users = User::create($request->all());
+        return $this->successResponse($users, Response::HTTP_CREATED);
+
+    }
+
+
+    public function show($id)
+    {
+        $users = User::where('userid', $id)->first();
+        if($users){
+            return $this->successResponse($users);
+        }
+    
+        {
+            return $this->errorResponse('User ID Does Not Exist', Response::HTTP_NOT_FOUND);
+
         }
 
-        public function getUsers(){
-            $users = User::all();
-            return response()->json($users, 200);
+    }
+    
+    public function update(Request $request, $id)
+    {
+
+        $rules = [
+
+            'username' => 'max:20',
+            'password' => 'max:20',
+            // 'gender' => 'required|in:Male,Female',
+
+        ];
+
+        $this->validate($request, $rules);
+
+        $users = User::findOrFail($id);
+
+        $users->fill($request->all());
+        
+        $users->save();
+        if($users){
+            return $this->successResponse($users);
+        }
+    
+        {
+            return $this->errorResponse('User ID Does Not Exist', Response::HTTP_NOT_FOUND);
+
         }
     }
+
+    public function delete($id)
+    {
+        $users = User::where('userid', $id)->first();
+        if($users){
+            $users->delete();
+            return $this->successResponse($users);
+        }
+    
+        {
+            return $this->errorResponse('User ID Does Not Exist', Response::HTTP_NOT_FOUND);
+
+        }
+
+    }    
+}
 
 ?>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ $router->get('/', function () use ($router) {
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->get('/users',['uses' => 'UserController@getUsers']);
 });
+
+// more simple routes
+$router->get('/users', 'UserController@index'); // get all users records
+$router->post('/users', 'UserController@add'); // create new user record
+$router->get('/users/{id}', 'UserController@show'); // get user by id
+$router->put('/users/{id}', 'UserController@update'); // update user record
+$router->patch('/users/{id}', 'UserController@update'); // update user record
+$router->delete('/users/{id}', 'UserController@delete'); // delete record
